86b36af36319dbd692d4a8dde00922d707e6b8e9
katherine55 <league@iMac.attlocal.net>
2018-07-08 11:32:35 -0700

    new
    lvl 4

---
+++ Anagramss.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class Anagramss {
+	ArrayList myList = new ArrayList();
+	ArrayList possibilities = new ArrayList();
+	String word;
+
+	public static void main(String[] args) throws FileNotFoundException {
+		Anagramss andy = new Anagramss();
+		andy.scanning();
+	}
+
+	void scrambler() throws FileNotFoundException {
+		word = JOptionPane.showInputDialog("Word with out spaces:");
+
+		for (int i = 0; i < word.length(); i++) {
+			myList.add(word.charAt(i));
+		}
+		
+		Scanner myScanner = new Scanner(new File("words.txt"));
+		while (myScanner.hasNextLine()) {
+
+		}
+
+	}
+
+
+}
