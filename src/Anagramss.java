a99cab3c0d81351910bd6be68166a6e45d78d4bf
katherine55 <league@iMac.attlocal.net>
2018-07-15 11:29:31 -0700

    NEW

--- src/Anagramss.java
+++ src/Anagramss.java
@@ -2,11 +2,13 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Stack;
+
 import javax.swing.JOptionPane;
 
 public class Anagramss {
-	ArrayList myList = new ArrayList();
-	ArrayList possibilities = new ArrayList();
+	Stack<Character> myList = new Stack<Character>();
+	ArrayList<String> possibilities = new ArrayList<String>();
 	String word;
 
 	public static void main(String[] args) throws FileNotFoundException {
@@ -20,13 +22,25 @@ public class Anagramss {
 		for (int i = 0; i < word.length(); i++) {
 			myList.add(word.charAt(i));
 		}
-		
-		Scanner myScanner = new Scanner(new File("words.txt"));
-		while (myScanner.hasNextLine()) {
 
+		Scanner myScanner = new Scanner(new File("src/words.txt"));
+		while (myScanner.hasNextLine()) {
+			String check = myScanner.nextLine();
+			for (int i = word.length() - 1; i > -1; i--) {
+				if (check.contains("" + word.charAt(i))) {
+					myList.pop();
+				}
+			}
+			if (myList.isEmpty()) {
+				possibilities.add(check);
+			}
+
+		}
+		System.out.println("Here are the anagrams for the word: " + word);
+		for (String x : possibilities) {
+			System.out.println(x);
 		}
 
 	}
-
 
 }
